62e3b46b08a0b0356234e82c6e3ca095bc622551
azad <mohammadazad.bc@gmail.com>
2024-01-24 22:43:37 +0100

    saying hello, and welcome

--- src/component/mainPage/MainPage.jsx
+++ src/component/mainPage/MainPage.jsx
@@ -2,14 +2,22 @@ import React, { useState } from 'react'
 import "./door.css"
 import leftDoor from "../../images/left.jpg"
 import righDoor from "../../images/right.jpg"
-import wlcpic from "../../images/wlcs.jpg"
+import wlcpic from "../../images/wlc.png"
+import wlcText from "../../images/wlcs.png"
 
 function MainPage() {
 
   const [isOpen, setIsOpen] = useState(false);
+  const [sayWlc, setSayWlc ] = useState(true)
+
   setInterval(()=>{
       setIsOpen(true)
-  },1000)
+    },1000)
+  if(isOpen == true){
+    setInterval(()=> {
+      setSayWlc(false)
+    },3000)
+  }
 
   return (
     <div className='mainPage'>
@@ -31,15 +39,20 @@ function MainPage() {
           </div>
       </div>
       
-      <div className="receip">
+      <div className={`receip ${sayWlc? "" : "receipNone"}`} >
 
+      
         <div className="recepPerson">
           <img style={{
             resizeMode:'cover',
             width:'700px',
             height:'700px'
           }} src={wlcpic} alt="not" />
+          <div className="wlcText">
+            <img src={wlcText} alt="" />
+          </div>
         </div>
+         
       </div>
 
 
