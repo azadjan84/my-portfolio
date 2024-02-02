baefbb0b549dc4b63e3fce2e53ae8f0bcda2e153
azad <mohammadazad.bc@gmail.com>
2024-02-02 19:26:07 +0100

    project section

--- src/component/myprojects/Projects.jsx
+++ src/component/myprojects/Projects.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import "./projects.css"
+import Slide from './slides/Slide'
 
 function Projects() {
   return (
     <div className='projectContainer'>
       <div className="pcBody">
-        
+          <div className="slides">
+              <div className="sLeft"></div>
+              <div className="sMiddle">
+                <div className="sMT">
+                  <Slide />
+                  <Slide />
+                  <Slide />
+                </div>
+                <div className="sMB">
+                <Slide />
+                <Slide />
+                <Slide />
+              </div>
+              </div>
+              
+              <div className="sRight"></div>
+          </div>
       </div>
+
     </div>
   )
 }
---
+++ src/component/myprojects/slides/Slide.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./slide.css"
+function Slide() {
+  return (
+    <div className='slideContainer'>Slide</div>
+  )
+}
+
+export default Slide
