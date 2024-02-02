2fd17f02d87f00f3d016799facfe0e4549946a6e
azad <mohammadazad.bc@gmail.com>
2024-02-03 00:09:56 +0100

    add projects data

---
+++ src/component/myprojects/data.js
@@ -0,0 +1,102 @@
+import moveiPoster from "./images/movie.png"
+import oldPortfolio from "./images/oldPortfolio.png"
+import moderUi from "./images/moderui.png"
+import FitnessImg from "./images/fitness.png"
+import ecommerceImg from "./images/ecomerce.png"
+import youtube from "./images/youtube.png"
+import booking from "./images/booking.png"
+import chat from "./images/chat.png"
+import jmr from "./images/jmr.png"
+
+const linkProjects = [
+
+    {
+        projectTile:"Ecommerce",
+        language:"HTML, CSS, JS",
+        onlineLink:"https://mohammadazadbc.github.io/Ecommerce/",
+        poster:ecommerceImg,
+        github:"https://github.com/Mohammadazadbc/Ecommerce?tab=readme-ov-file",
+        description:"This is an E-commerce project built with HTML, CSS, and JS. I found the design on Google, and while it is not fully functional, it features an amazing design. It was a valuable project in terms of learning and provided a great opportunity for skill development."
+    },
+   
+         {
+        projectTile:"My old Portfolio",
+        language:"React & Node JS",
+        onlineLink:"https://azad-portfolio-pro.netlify.app/",
+        poster:oldPortfolio,
+        github:"https://github.com/Mohammadazadbc/My-Portfolio?tab=readme-ov-file",
+        description:"This is my old portfolio built with React and Node.js. I copied the design and rebuilt it following a tutorial from JavaScript Mastery. It is a truly amazing project, though it holds more significance in terms of learning rather than the satisfaction of using my creativity to construct a remarkable piece of work. This realization prompted me to start building my new portfolio, relying on my imagination and drawing inspiration from various aspects of life."
+    },
+    {
+        projectTile:"Website with Modern UI/UX",
+        language:"React & Tailwind",
+        onlineLink:"https://modern-ui-ux-app.netlify.app/",
+        poster:moderUi,
+        github:"https://github.com/Mohammadazadbc/Modern-ui-ux-app",
+        description:"This is an amazing project built with React and Tailwind CSS technology. The design is inspired by JavaScript Mastery, and as I constructed this project, it became an incredible endeavor. It represents a futuristic website with stunning design elements."
+    },
+    {
+        projectTile:"Fitness Exercises App ",
+        language:"React & RapidAPI",
+        onlineLink:"https://azad-gym.netlify.app/",
+        poster:FitnessImg,
+        github:"https://github.com/Mohammadazadbc/Fitness-Exercises-App?tab=readme-ov-file",
+        description:"This is an amazing project built with React and RapidAPI. On this website, you can discover exercises for each part of the body presented in GIF format. The exercises are categorized by the corresponding body part, providing detailed instructions on how to perform them. Additionally, there is a search bar that allows you to find specific exercises for particular areas of the body."
+    },
+    {
+        projectTile:"Ecommerce",
+        language:"HTML, CSS, JS",
+        onlineLink:"https://mohammadazadbc.github.io/Ecommerce/",
+        poster:ecommerceImg,
+        github:"https://github.com/Mohammadazadbc/Ecommerce?tab=readme-ov-file",
+        description:"This is an E-commerce project built with HTML, CSS, and JS. I found the design on Google, and while it is not fully functional, it features an amazing design. It was a valuable project in terms of learning and provided a great opportunity for skill development."
+    },
+
+    
+
+]
+
+
+const unLinkProjects =[
+    {
+        projectTile:"Movie Trailer Showcase",
+        language:"React & Spring boot",
+        onlineLink:"",
+        poster:moveiPoster,
+        github:"https://github.com/Mohammadazadbc/movies",
+        description:"This project, developed with Spring Boot and React, leverages MongoDB as its database. It retrieves movie details from the database, showcasing names, posters, and concise descriptions. Each movie entry is equipped with a clickable button for viewing the trailer, and users can contribute reviews for the films."
+    },
+    {
+        projectTile:"YouTube Clone Application ",
+        language:"React CSS",
+        onlineLink:"",
+        poster:youtube,
+        github:"https://github.com/Mohammadazadbc/Media-yt",
+        description:"modern web and responsive build with React JS application consisting of stunning video sections, custom categories, channel pages, and, most importantly, you can play videos straight from your YouTube Clone App!",
+    },
+    {
+        projectTile:"Booking website",
+        language:"React Node JS MongoDB",
+        onlineLink:"",
+        poster:booking,
+        github:"https://github.com/Mohammadazadbc/Booking-App",
+        description:"This is a full-stack project with React and Node.js. It is an absolutely amazing project that allows you to book hotels. You can search for hotels in different cities, filter by price, number of people, date, and the number of rooms. You can also create an account or log in, and then proceed to reserve a room.",
+    },
+    {
+        projectTile:"Chat App",
+        language:"JS Node JS",
+        onlineLink:"",
+        poster:chat,
+        github:"https://github.com/Mohammadazadbc/Realtime-chat-React-Node-Socket.io",
+        description: "It is a project built with Node.js and Socket.IO that allows two users to chat together in real-time. In this project, more focus is placed on functionality rather than design." ,
+    },
+    {
+        projectTile:"Proceskoeling website",
+        language:"CSS React",
+        onlineLink:"",
+        poster:jmr,
+        github:"https://github.com/Mohammadazadbc/jmr-website",
+        description:"This was a project that I developed for a friend who started a new business and asked me to create a website for him. I completed a significant portion of the website at the time, but I faced challenges in putting it online with a good link and making it discoverable on Google for people to find.",
+    },
+]
+export default linkProjects
