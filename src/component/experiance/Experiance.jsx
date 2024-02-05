d47b9deb47c06dc456818b26d8f33da1d25a6b99
azad <mohammadazad.bc@gmail.com>
2024-02-05 16:46:36 +0100

    skills page tow side

--- src/component/experiance/Experiance.jsx
+++ src/component/experiance/Experiance.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import "./exper.css"
-
+import experiances from './expData'
 function Experiance() {
+
+    console.log(experiances)
   return (
-    <div>Experiance</div>
+    <div className='exContainer' >
+        <div className="exBody">
+            <div className="exLeft">
+                <div className="experianceTitle"> Experiences</div>
+                <div className="expeMenu">
+                    <ul>
+                        { experiances.map(item =>(
+                            <li>{item.workTitle} </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            <div className="exRight"></div>
+        </div>
+
+    </div>
   )
 }
 
