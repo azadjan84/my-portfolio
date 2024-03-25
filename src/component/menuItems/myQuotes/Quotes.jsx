acf65078fb19e754f77665d233eedeae46df2d82
azad <mohammadazad.bc@gmail.com>
2024-03-25 21:19:45 +0100

    quotes

--- src/component/menuItems/myQuotes/Quotes.jsx
+++ src/component/menuItems/myQuotes/Quotes.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react'
 import "./quotes.css"
-import leftIcon from "./images/left.png"
-import rightIcon from "./images/right.png"
 import redFlower from "./images/redFlower.jpg"
 function Quotes({setNotBuildPage}) {
+    
     const [currentIndex, setCurrentIndex] = useState(0);
     const quotesData = [
         {quot :"Your difference is your strength, not your weakness.", quotImg:redFlower},
@@ -19,15 +18,13 @@ function Quotes({setNotBuildPage}) {
     <div className='quotes'>
         <div className="backmark" onClick={()=>goBackFun()} > {backMark} Back </div>
         <div className="quotesContainer">
-            <div className="quotesBox"> 
-            <button className='leftBtn'> <img src={rightIcon} alt="" /> {leftIcon} </button>
-            <button className='rightBtn'> <img src={leftIcon} alt="" />  </button>
-              <div className="quot" >
-                    <div className="quoteCenter">
-                    <img src={quotesData[0].quotImg} alt="" />
-                    </div>
-                </div>
-            </div>
+            <div className="qcdiv"> <p>"Your difference is your strength, not your weakness."</p> </div>
+            <div className="qcdiv"> <p>"If I were to switch places with a friend for a week to observe my communication skills, emotional intelligence, and professional development from an external perspective, it could provide valuable insights for growth."</p> </div>
+            <div className="qcdiv"> <p>"Don't wait for everything to be perfect to start something."</p> </div>
+            <div className="qcdiv"> <p>"Despite our efforts to make things go smoothly, many aspects of the world are beyond our control. I accept that anything can happen. Therefore, I believe in the importance of resilience, getting back up, and starting fresh each day."</p> </div>
+            
+          
+           
         </div>
     </div>
   )
