f37e1801358ef074580f3915e38b80a7e41c2033
azad <mohammadazad.bc@gmail.com>
2024-04-07 12:38:24 +0200

    add read me

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-
 [My Portfolio](https://azadjan84.github.io/my-portfolio/)
+
+Check out my new portfolio! I poured my heart and soul into this one—it's my best work yet. I started from scratch, making sure it looks clean and user-friendly. And I didn't just focus on the surface stuff. I also made sure the code behind it is top-notch, clean, and optimized as much as possible. Take a look!
--- src/component/myprojects/data.js
+++ src/component/myprojects/data.js
@@ -106,7 +106,7 @@ const linkProjects = [
         logo:portfolio1,
         poster:newPortfolio,
         github:"https://github.com/azadjan84/my-portfolio",
-        description:"This is my new portfolio, and it's the most complete one that I have built with so much passion and hard work. I built it from scratch, drawing inspiration for the design from real-world cities."
+        description:"Check out my new portfolio! I poured my heart and soul into this one—it's my best work yet. I started from scratch, making sure it looks clean and user-friendly. And I didn't just focus on the surface stuff. I also made sure the code behind it is top-notch, clean, and optimized as much as possible. Take a look!"
     },
 
     
